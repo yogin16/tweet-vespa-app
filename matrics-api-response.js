131f9cb63f7feb9fced7d70e974ce7e020ebf2d8
Yogin <yogin.daiict@gmail.com>
2017-10-27 12:22:55 +0530

    Create matrics-api-response.js

---
+++ matrics-api-response.js
@@ -0,0 +1,563 @@
+//curl http://localhost:8080/state/v1/metrics
+{
+    "metrics": {
+        "snapshot": {
+            "from": 1.50908700754E9,
+            "to": 1.50908706754E9
+        },
+        "values": [
+            {
+                "name": "search_connections",
+                "values": {
+                    "average": 0,
+                    "count": 1,
+                    "last": 0,
+                    "max": 0,
+                    "min": 0,
+                    "rate": 0.016666666666666666
+                }
+            },
+            {
+                "name": "serverThreadPoolSize",
+                "values": {
+                    "average": 500,
+                    "count": 599,
+                    "last": 500,
+                    "max": 500,
+                    "min": 0,
+                    "rate": 9.983333333333333
+                }
+            },
+            {
+                "name": "serverActiveThreads",
+                "values": {
+                    "average": 0,
+                    "count": 599,
+                    "last": 0,
+                    "max": 0,
+                    "min": 0,
+                    "rate": 9.983333333333333
+                }
+            },
+            {
+                "name": "freeMemoryBytes",
+                "values": {
+                    "average": 1.3635148826666667E9,
+                    "count": 6,
+                    "last": 1.365337312E9,
+                    "max": 1.370051584E9,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "jdisc.open_file_descriptors",
+                "values": {
+                    "average": 271,
+                    "count": 6,
+                    "last": 271,
+                    "max": 271,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "mem.heap.total",
+                "values": {
+                    "average": 1.530134528E9,
+                    "count": 6,
+                    "last": 1.530134528E9,
+                    "max": 1.530134528E9,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "proc.uptime",
+                "values": {
+                    "average": 1.3752713E8,
+                    "count": 30,
+                    "last": 1.3751813E8,
+                    "max": 1.3755613E8,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            },
+            {
+                "name": "usedMemoryBytes",
+                "values": {
+                    "average": 1.6661964533333334E8,
+                    "count": 6,
+                    "last": 1.64797216E8,
+                    "max": 1.713268E8,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "serverStartedMillis",
+                "values": {
+                    "average": 1.3752713E8,
+                    "count": 30,
+                    "last": 1.3751813E8,
+                    "max": 1.3755613E8,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            },
+            {
+                "name": "totalMemoryBytes",
+                "values": {
+                    "average": 1.530134528E9,
+                    "count": 6,
+                    "last": 1.530134528E9,
+                    "max": 1.530134528E9,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "jdisc.deactivated_containers.total",
+                "values": {
+                    "average": 1,
+                    "count": 6,
+                    "last": 1,
+                    "max": 1,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "jdisc.memory_mappings",
+                "values": {
+                    "average": 1812,
+                    "count": 6,
+                    "last": 1812,
+                    "max": 1812,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "http.status.1xx",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "name": "http.status.2xx",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "name": "http.status.3xx",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "name": "http.status.4xx",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "name": "mem.heap.used",
+                "values": {
+                    "average": 1.6661964533333334E8,
+                    "count": 6,
+                    "last": 1.64797216E8,
+                    "max": 1.713268E8,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "http.status.5xx",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "name": "active_queries",
+                "values": {
+                    "average": 0,
+                    "count": 1,
+                    "last": 0,
+                    "max": 0,
+                    "min": 0,
+                    "rate": 0.016666666666666666
+                }
+            },
+            {
+                "name": "mem.heap.free",
+                "values": {
+                    "average": 1.3635148826666667E9,
+                    "count": 6,
+                    "last": 1.365337312E9,
+                    "max": 1.370051584E9,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "name": "jdisc.deactivated_containers.with_retained_refs",
+                "values": {
+                    "average": 0,
+                    "count": 6,
+                    "last": 0,
+                    "max": 0,
+                    "min": 0,
+                    "rate": 0.1
+                }
+            },
+            {
+                "dimensions": {"source": "content-root-cluster"},
+                "name": "error.timeout",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "query_latency",
+                "values": {
+                    "average": 20001,
+                    "count": 0,
+                    "last": 20001,
+                    "max": 20001,
+                    "min": 20001,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "failed_queries",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "empty_results",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "max_query_latency",
+                "values": {
+                    "average": 20001,
+                    "count": 0,
+                    "last": 20001,
+                    "max": 20001,
+                    "min": 20001,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "totalhits_per_query",
+                "values": {
+                    "average": 0,
+                    "count": 0,
+                    "last": 0,
+                    "max": 0,
+                    "min": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "hits_per_query",
+                "values": {
+                    "average": 0,
+                    "count": 0,
+                    "last": 0,
+                    "max": 0,
+                    "min": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "peak_qps",
+                "values": {
+                    "average": 0.020838542969075603,
+                    "count": 0,
+                    "last": 0.020838542969075603,
+                    "max": 0.020838542969075603,
+                    "min": 0.020838542969075603,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "queries",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {"chain": "vespa"},
+                "name": "mean_query_latency",
+                "values": {
+                    "average": 20001,
+                    "count": 0,
+                    "last": 20001,
+                    "max": 20001,
+                    "min": 20001,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverNumRequests",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "http.out.firstbytetime",
+                "values": {
+                    "average": 20002,
+                    "count": 0,
+                    "last": 20002,
+                    "max": 20002,
+                    "min": 20002,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "http.latency",
+                "values": {
+                    "average": 20004,
+                    "count": 0,
+                    "last": 20004,
+                    "max": 20004,
+                    "min": 20004,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverConnectionsOpenMax",
+                "values": {
+                    "average": 33,
+                    "count": 30,
+                    "last": 33,
+                    "max": 33,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverNumConnections",
+                "values": {
+                    "average": 17557,
+                    "count": 30,
+                    "last": 17557,
+                    "max": 17557,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverConnectionDurationMean",
+                "values": {
+                    "average": 16129.662755596048,
+                    "count": 30,
+                    "last": 16129.662755596059,
+                    "max": 16129.662755596059,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverConnectionDurationMax",
+                "values": {
+                    "average": 370760,
+                    "count": 30,
+                    "last": 370760,
+                    "max": 370760,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverTimeToFirstByte",
+                "values": {
+                    "average": 20002,
+                    "count": 0,
+                    "last": 20002,
+                    "max": 20002,
+                    "min": 20002,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverBytesSent",
+                "values": {
+                    "average": 472,
+                    "count": 0,
+                    "last": 472,
+                    "max": 472,
+                    "min": 472,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverNumSuccessfulResponseWrites",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "http.out.bytes",
+                "values": {
+                    "average": 472,
+                    "count": 0,
+                    "last": 472,
+                    "max": 472,
+                    "min": 472,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverTotalSuccessfulResponseLatency",
+                "values": {
+                    "average": 20004,
+                    "count": 0,
+                    "last": 20004,
+                    "max": 20004,
+                    "min": 20004,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "jdisc.http.requests",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverNumSuccessfulResponses",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "http.requests",
+                "values": {
+                    "count": 0,
+                    "rate": 0
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverNumOpenConnections",
+                "values": {
+                    "average": 0,
+                    "count": 30,
+                    "last": 0,
+                    "max": 0,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            },
+            {
+                "dimensions": {
+                    "serverName": "SearchServer",
+                    "serverPort": "8080"
+                },
+                "name": "serverConnectionDurationStdDev",
+                "values": {
+                    "average": 21233.44277178741,
+                    "count": 30,
+                    "last": 21233.442771787413,
+                    "max": 21233.442771787413,
+                    "min": 0,
+                    "rate": 0.5
+                }
+            }
+        ]
+    },
+    "status": {"code": "up"},
+    "time": 1509087068206
+}
